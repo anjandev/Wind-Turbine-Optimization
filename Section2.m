486b10e42d32ce4d8b0a43daccd5b223d35a625b
Anjan Momi <anjan@momi.ca>
2017-03-31 19:07:37 -0700

    Finished section 2

--- Section2.m
+++ Section2.m
@@ -6,6 +6,9 @@ addpath './obj'
 
 DIVISIONS = 10000;
 
+MAX_DOMAIN_OMEGA = 9.55;
+MIN_DOMAIN_OMEGA = 2;
+
 WINDSPEED = 12; % m/s
 
 MIN_ROTOR_RADIUS = 1; % m
@@ -16,3 +19,26 @@ MAX_ROTOR_SPEED_HZ = 35; % Hz
 
 rotor_radiuses = linspace(MIN_ROTOR_RADIUS, MAX_ROTOR_RADIUS, DIVISIONS);
 rotor_speeds = linspace(MIN_ROTOR_SPEED_HZ, MAX_ROTOR_SPEED_HZ, DIVISIONS);
+
+n = 1;
+
+CpMax = 0;
+rotorRWithMaxCp = 0;
+rotorSpeedWithMaxCp = 0;
+advancedRatioWithMaxCp = 0;
+
+for rotorRadiusIdx = 1:DIVISIONS
+    for rotorSpeedIdx = 1:DIVISIONS
+        advancedRatio = rotor_radiuses(rotorRadiusIdx) * rotor_speeds(rotorSpeedIdx) / WINDSPEED;
+        if advancedRatio >= MIN_DOMAIN_OMEGA && advancedRatio <= MAX_DOMAIN_OMEGA
+            if CpVsOmega(advancedRatio) > CpMax
+                CpMax = CpVsOmega(advancedRatio);
+                rotorRWithMaxCp = rotor_radiuses(rotorRadiusIdx);
+                rotorSpeedWithMaxCp = rotor_speeds(rotorSpeedIdx);
+                advancedRatioWithMaxCp = advancedRatio;
+            end
+        end
+    end
+ end
+
+ advancedRatioWithMaxCp
